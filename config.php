d5e5fbb5d0acf48bd6111448d9bf7a0f398aa77e
Georgemabeyo <149334413+Georgemabeyo@users.noreply.github.com>
2025-08-10 20:23:44 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,13 +2,16 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$host = getenv('DB_HOST') ?: 'dpg-d2c7s795pdvs73dcpo6g-a';  
+// Credentials za PostgreSQL
+$host = getenv('DB_HOST') ?: 'dpg-d2c7s795pdvs73dcpo6g-a.oregon-postgres.render.com';
 $port = getenv('DB_PORT') ?: '5432';
 $dbname = getenv('DB_NAME') ?: 'ukoo';
 $user = getenv('DB_USER') ?: 'makomelelo';
 $password = getenv('DB_PASS') ?: 'HeONu12TjSP7NJHeXwMnwdOnzarQ3KvH';
+$sslmode = getenv('DB_SSLMODE') ?: 'require'; // Render mara nyingi inahitaji SSL
 
-$conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+// Unganisha PostgreSQL
+$conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password sslmode=$sslmode";
 $conn = pg_connect($conn_string);
 
 if (!$conn) {
