781bbe86494c92fda7194910a2f5b2e1638dc3c0
Georgemabeyo <149334413+Georgemabeyo@users.noreply.github.com>
2025-08-10 14:01:21 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv('DB_HOST') ?: '127.0.0.1';
-$user = getenv('DB_USER') ?: 'root';
-$password = getenv('DB_PASS') ?: '';
-$database = getenv('DB_NAME') ?: 'ukoo';
-$port = getenv('DB_PORT') ?: 3306;
+$host = getenv('DB_HOST');         // Hii itasoma environment variable DB_HOST
+$user = getenv('DB_USER');         // Environment variable DB_USER
+$password = getenv('DB_PASS');     // Environment variable DB_PASS
+$database = getenv('DB_NAME');     // Environment variable DB_NAME
+$port = getenv('DB_PORT') ?: 3306; // Optional: default port 3306
 
 $conn = new mysqli($host, $user, $password, $database, $port);
 
