ee39e2c83dbcfdf15908378898fcbac25a2a65e8
Georgemabeyo <149334413+Georgemabeyo@users.noreply.github.com>
2025-08-17 15:23:16 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -32,7 +32,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <title>Ukoo wa Makomelelo | Mawasiliano</title>
-<link rel="stylesheet" href="assets/css/style.css" />
+<link rel="stylesheet" style.css" />
 </head>
 <body class="light-mode">
 
